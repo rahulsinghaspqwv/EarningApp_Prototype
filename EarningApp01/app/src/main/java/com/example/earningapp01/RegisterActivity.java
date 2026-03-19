4a0425f6c00ba20fc5564bb6a55cf8c4ec53b758
rahulsinghaspqwv <rahulsinghaspqwv@gmail.com>
2026-03-19 10:26:57 +0530

    Adding SignIn Activity

--- EarningApp01/app/src/main/java/com/example/earningapp01/RegisterActivity.java
+++ EarningApp01/app/src/main/java/com/example/earningapp01/RegisterActivity.java
@@ -1,8 +1,14 @@
 package com.example.earningapp01;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +18,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText etUsername, etEmail, etPassword, etConfirmPassword;
+    Button btnRegister;
+    ProgressBar progressBar;
+    TextView tvForgetPassword, tvAlreadyHaveAnAccount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
+        // Immersive UI
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             getWindow().setDecorFitsSystemWindows(false);
         }
@@ -26,5 +38,61 @@ public class RegisterActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // Initialize views method calling
+        init();
+        // Register Button Click
+        btnRegister.setOnClickListener(v -> registerUser());
+        tvAlreadyHaveAnAccount.setOnClickListener(v -> tvAlreadyHaveAnAccount());
+
+    }
+    private void init(){
+        // Initialize views
+        etUsername = findViewById(R.id.etUsername);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        etConfirmPassword = findViewById(R.id.etConfirmPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+        progressBar = findViewById(R.id.progressBar);
+        tvAlreadyHaveAnAccount = findViewById(R.id.tvGoSignIn);
+    }
+    private void registerUser(){
+        String username = etUsername.getText().toString().trim();
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+        String confirmPassword = etConfirmPassword.getText().toString().trim();
+        // validation
+        if (username.isEmpty()){
+            etUsername.setError("Username required");
+            etUsername.requestFocus();
+            return;
+        }
+        if (email.isEmpty()){
+            etEmail.setError("Email required");
+            etEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            etEmail.setError("Enter valid email");
+            etEmail.requestFocus();
+            return;
+        }
+        if (password.isEmpty()){
+            etPassword.setError("Password required");
+            etPassword.requestFocus();
+            return;
+        }
+        if (password.length() < 6){
+            etPassword.setError("Minimum 6 characters");
+            etPassword.requestFocus();
+            return;
+        }
+        if (!password.equals(confirmPassword)){
+            etConfirmPassword.setError("Password do not match");
+            etConfirmPassword.requestFocus();
+            return;
+        }
+    }
+    private void tvAlreadyHaveAnAccount(){
+        startActivity(new Intent(this,SignInActivity.class));
     }
 }
---
+++ EarningApp01/app/src/main/java/com/example/earningapp01/SignInActivity.java
@@ -0,0 +1,80 @@
+package com.example.earningapp01;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SignInActivity extends AppCompatActivity {
+    EditText etEmail, etPassword;
+    Button btnLogin;
+    ProgressBar progressBar;
+    TextView tvForget, tvRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_in);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            getWindow().setDecorFitsSystemWindows(false);
+        }
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        init();
+        // Login Button
+        btnLogin.setOnClickListener(v -> loginUser());
+        tvRegister.setOnClickListener(v -> goToRegister());
+        tvForget.setOnClickListener(v -> forgetPassword());
+    }
+    private void init(){
+        etEmail = findViewById(R.id.etLoginEmail);
+        etPassword = findViewById(R.id.etLoginPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        progressBar = findViewById(R.id.loginProgressBar);
+        tvForget = findViewById(R.id.tvLoginForgetPassword);
+        tvRegister = findViewById(R.id.tvGoRegister);
+    }
+    private void loginUser(){
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+        if (email.isEmpty()){
+            etEmail.setError("Email required");
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            etEmail.setError("Invalid Email");
+            return;
+        }
+        if (password.isEmpty()){
+            etPassword.setError("Password required");
+            return;
+        }
+    }
+    // Forget password
+    private void forgetPassword(){
+        String email = etEmail.getText().toString().trim();
+        if (email.isEmpty()){
+            etEmail.setError("Enter email First");
+            return;
+        }
+    }
+    private void goToRegister(){
+        startActivity(new Intent(this, RegisterActivity.class));
+    }
+}
