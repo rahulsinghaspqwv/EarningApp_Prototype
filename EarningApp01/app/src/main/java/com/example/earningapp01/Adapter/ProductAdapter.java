71e9c1c62e256a0700b7c1706599166aae969b4b
rahulsinghaspqwv <rahulsinghaspqwv@gmail.com>
2026-03-31 15:15:46 +0530

    current remaining update

--- EarningApp01/app/src/main/java/com/example/earningapp01/Adapter/ProductAdapter.java
+++ EarningApp01/app/src/main/java/com/example/earningapp01/Adapter/ProductAdapter.java
@@ -1,10 +1,19 @@
 package com.example.earningapp01.Adapter;
 
 import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.earningapp01.Product;
+import com.example.earningapp01.R;
 
 import java.util.ArrayList;
 
@@ -15,4 +24,34 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
         this.context = context;
         this.productList = productList;
     }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.product_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Product product = productList.get(position);
+        holder.name.setText(product.getName());
+        holder.price.setText(product.getPrice());
+        Glide.with(context).load(product.getImageUrl()).into(holder.image);
+    }
+
+    public int getItemCount(){
+        return productList.size();
+    }
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        TextView name, price;
+        ImageView image;
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+            name = itemView.findViewById(R.id.productName);
+            price = itemView.findViewById(R.id.productPrice);
+            image = itemView.findViewById(R.id.productImage);
+        }
+    }
 }
+
--- EarningApp01/app/src/main/java/com/example/earningapp01/MainActivity.java
+++ EarningApp01/app/src/main/java/com/example/earningapp01/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.earningapp01;
 
+import android.annotation.SuppressLint;
+import android.app.DownloadManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.earningapp01.Adapter.ProductAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ArrayList<Product> productList;
+    ProductAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +47,64 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        productList = new ArrayList<>();
+        adapter = new ProductAdapter(this, productList);
+        recyclerView.setAdapter(adapter);
+        fetchProductsFromDatabase(); // IMPORTANT
+
+    }
+    private void fetchProductsFromDatabase() {
+        String url = "https://megdeal.infinityfreeapp.com/getProducts.php?api=1";
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+
+        StringRequest request = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONArray array = new JSONArray(response);
+
+                            for (int i = 0; i < array.length(); i++) {
+
+                                JSONObject obj = array.getJSONObject(i);
+
+                                Product product = new Product(
+                                        obj.getString("name"),
+                                        obj.getString("price"),
+                                        obj.getString("image")
+                                );
+
+                                productList.add(product);
+                            }
+
+                            adapter.notifyDataSetChanged();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<String, String>();
+                headers.put("User-Agent", "Mozilla/5.0");
+                headers.put("Accept", "application/json");
+                return headers;
+            }
+        };
+
+        queue.add(request);
     }
 }
